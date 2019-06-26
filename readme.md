89fce67d1768bc10ed455e0b5c8fbba8af094d04
Antoni Kepinski <xxczaki@pm.me>
2019-06-26 18:23:25 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,154 @@
+# Welding Utils [![Build Status](https://travis-ci.org/xxczaki/welding-utils.svg?branch=master)](https://travis-ci.org/xxczaki/welding-utils) [![XO code style](https://img.shields.io/badge/code_style-XO-5ed9c7.svg)](https://github.com/xojs/xo)
+
+> Useful utilities related to welding 🛠️
+
+## Highlights
+
+- Simple API
+- 0 dependencies
+- Written in TypeScript
+
+## Install
+
+```
+$ npm install welding-utils
+```
+
+## Usage
+
+```js
+const {heatInput} = require('./dist');
+
+const options = {
+    voltage: 200,
+	amperage: 32,
+	efficiencyFactor: 0.8,
+	lenght: 20,
+	time: 11
+}
+
+console.log(heatInput(options)); //=> 2.8160000000000003
+```
+
+## API
+
+### heatInput(options)
+
+**options**
+
+Type: `Object`
+
+```ts
+interface Options {
+	voltage: number;
+	amperage: number;
+	efficiencyFactor: number;
+	lenght: number;
+	time: number;
+}
+```
+
+Returns equivalent carbon content (CET)
+
+### ceq(elements)
+
+**elements**
+
+Type: `Object`
+
+```ts
+interface Elements {
+	coal: number;
+	manganese: number;
+	chromium: number;
+	molybdenum: number;
+	vanadium: number;
+	nickel: number;
+	copper: number;
+}
+```
+
+Returns equivalent carbon content (CET)
+
+### cet(elements)
+
+**elements**
+
+Type: `Object`
+
+```ts
+interface Elements {
+	coal: number;
+	manganese: number;
+	chromium: number;
+	molybdenum: number;
+	vanadium: number;
+	nickel: number;
+	copper: number;
+}
+```
+
+Returns equivalent carbon content (CET)
+
+### ceAws(elements)
+
+**elements**
+
+Type: `Object`
+
+```ts
+interface Elements {
+	coal: number;
+	manganese: number;
+	chromium: number;
+	molybdenum: number;
+	vanadium: number;
+	nickel: number;
+	copper: number;
+	silicon: number;
+}
+```
+
+Returns equivalent carbon content (CE AWS)
+
+### pcm(elements)
+
+**elements**
+
+Type: `Object`
+
+```ts
+interface Elements {
+	coal: number;
+	manganese: number;
+	chromium: number;
+	molybdenum: number;
+	vanadium: number;
+	nickel: number;
+	copper: number;
+	silicon: number;
+	boron: number;
+}
+```
+
+Returns critical metal parameter (PCM)
+
+### pren(elements)
+
+**elements**
+
+Type: `Object`
+
+```ts
+interface Elements {
+	chromium: number;
+	molybdenum: number;
+	nitrogen: number;
+}
+```
+
+Returns pitting resistance equivalent number (PREN)
+
+## License
+
+MIT © [Antoni Kepinski](https://kepinski.me)
---
+++ source/index.ts
@@ -0,0 +1,135 @@
+'use strict';
+
+interface Options {
+	voltage: number;
+	amperage: number;
+	efficiencyFactor: number;
+	lenght: number;
+	time: number;
+}
+
+interface Elements {
+	coal: number;
+	manganese: number;
+	chromium: number;
+	molybdenum: number;
+	vanadium: number;
+	nickel: number;
+	copper: number;
+	silicon: number;
+	boron: number;
+	nitrogen: number;
+}
+
+type ceq = Omit<Elements, 'silicon' | 'boron' | 'nitrogen'>;
+type cet = Omit<Elements, 'silicon' | 'boron' | 'nitrogen'>;
+type ceAws = Omit<Elements, 'boron' | 'nitrogen'>;
+type pcm = Omit<Elements, 'nitrogen'>;
+type pren = Pick<Elements, 'chromium' | 'molybdenum' | 'nitrogen'>;
+
+/**
+    * @returns {number} Equivalent carbon content (CEQ)
+	*/
+const ceq = (elements: ceq): number => {
+	const {
+		coal,
+		manganese,
+		chromium,
+		molybdenum,
+		vanadium,
+		nickel,
+		copper
+	} = elements;
+
+	return (coal + (manganese / 6) + (chromium + molybdenum + vanadium) / 5 + (nickel + copper) / 15);
+};
+
+/**
+    * @returns {number} Equivalent carbon content (CET)
+	*/
+const cet = (elements: cet): number => {
+	const {
+		coal,
+		manganese,
+		chromium,
+		molybdenum,
+		nickel,
+		copper
+	} = elements;
+
+	return (coal + (manganese + molybdenum) / 10 + (chromium + copper) / 20 + (nickel / 40));
+};
+
+/**
+    * @returns {number} Equivalent carbon content (CE AWS)
+	*/
+const ceAws = (elements: ceAws): number => {
+	const {
+		coal,
+		manganese,
+		chromium,
+		molybdenum,
+		vanadium,
+		nickel,
+		copper,
+		silicon
+	} = elements;
+
+	return (coal + (manganese / 6) + (chromium + molybdenum + vanadium) / 5 + (nickel + copper) / 15 + (silicon / 6));
+};
+
+/**
+    * @returns {number} Critical metal parameter (PCM)
+	*/
+const pcm = (elements: pcm): number => {
+	const {
+		coal,
+		manganese,
+		chromium,
+		molybdenum,
+		vanadium,
+		nickel,
+		copper,
+		silicon,
+		boron
+	} = elements;
+
+	return (coal + (silicon / 30) + (manganese + copper + chromium) / 20 + (nickel / 60) + (molybdenum / 15) + (vanadium / 10) + 5 * boron);
+};
+
+/**
+    * @returns {number} Pitting resistance equivalent number (PREN)
+	*/
+const pren = (elements: pren): number => {
+	const {
+		chromium,
+		molybdenum,
+		nitrogen
+	} = elements;
+
+	return (chromium + (3.3 * molybdenum) + (16 * nitrogen));
+};
+
+/**
+    * @returns {number} Heat input
+	*/
+const heatInput = (options: Options): number => {
+	const {
+		voltage,
+		amperage,
+		efficiencyFactor,
+		lenght,
+		time
+	} = options;
+
+	return ((voltage * amperage * efficiencyFactor) / (lenght / time * 1000));
+};
+
+export {
+	ceq,
+	cet,
+	ceAws,
+	pcm,
+	pren,
+	heatInput
+};
